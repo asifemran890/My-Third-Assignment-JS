ebaba268f20e9beb92518999b75f7137986ba3ba
asifemran890 <freelancerasif890@gmail.com>
2025-08-09 18:25:37 +0600

    develoed

--- problem4.js
+++ problem4.js
@@ -1,9 +1,21 @@
-/** Problem 04 - (Delete / Store) */
-var fileName = "result.pdf";
+// /** Problem 04 - (Delete / Store) */
+// var fileName = "1.jpg";
+// //write your code here
+// if (
+//   fileName.includes("pdfData.jpg" || "docx.txt" || "docx.xpdf" || "slipdf.txt")
+// ) {
+//   console.log("Delete");
+// } else fileName.includes("result.pdf" || "data.docx" || "#exp.mp4");
+// console.log("Store");
+
+var fileName = "docx.txt";
 //write your code here
 if (
-  fileName.includes("pdfData.jpg" || "docx.txt" || "docx.xpdf" || "slipdf.txt")
+  fileName.endsWith(".pdf") ||
+  fileName.endsWith(".docx") ||
+  fileName.startsWith("#")
 ) {
+  console.log("Store");
+} else {
   console.log("Delete");
-} else fileName.includes("result.pdf" || "data.docx" || "#exp.mp4");
-console.log("Store");
+}
